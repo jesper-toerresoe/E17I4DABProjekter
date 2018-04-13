05c6020a2bfd6ca08ffb77f2e13ad33b1ae8f121
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2018-04-13 11:19:13 +0200

    Små tilføjelser

--- CraftManDB/Script1.sql
+++ CraftManDB/Script1.sql
@@ -1,10 +1,11 @@
 ﻿SELECT * FROM Håndværker
 SELECT * FROM Værktøjskasse
-SELECT*FROM Værktøj
+SELECT * FROM Værktøj
 SELECT * FROM Håndværker
+SELECT [Efternavn],[Ansættelsedato],[Fagområde],[Fornavn] FROM Håndværker WHERE ([HåndværkerId] = 1)
 
 SELECT        Håndværker.HåndværkerId, Håndværker.Ansættelsedato, Håndværker.Efternavn, Håndværker.Fagområde, Håndværker.Fornavn, Værktøjskasse.VKasseId, Værktøjskasse.Anskaffet, Værktøjskasse.Fabrikat, Værktøj.VærktøjsId, 
-                         Værktøj.Anskaffet AS Expr1, Værktøj.Fabrikat AS Expr2, Værktøj.Model, Værktøj.Serienr, Værktøj.Type
+                         Værktøj.Anskaffet AS VKTAnskaf, Værktøj.Fabrikat AS Expr2, Værktøj.Model, Værktøj.Serienr, Værktøj.Type
 FROM            Håndværker LEFT OUTER JOIN
                          Værktøjskasse ON Håndværker.HåndværkerId = Værktøjskasse.Håndværker LEFT OUTER JOIN
                          Værktøj ON Værktøjskasse.VKasseId = Værktøj.Værktøjskasse
